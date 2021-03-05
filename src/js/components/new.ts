b6200d86a320e4af30765440a97795670d61dc6c
scotch <liamcorbett01@gmail.com>
2021-03-05 12:23:41 +1000

    Add value checks and more animations

--- src/js/components/new.ts
+++ src/js/components/new.ts
@@ -91,12 +91,18 @@ class SelectFaction extends LitElement {
     selectedFactions: { [faction: string]: boolean } = {};
     updateParent: any;
     next: () => void;
+    maximumFactions: number;
+    firstRender: boolean;
+    recentlyUnselectedFaction: string;
 
     constructor(next: () => void, selectedFactions: { [faction: string]: boolean }, updateParent: any) {
         super();
         this.next = next;
         this.selectedFactions = selectedFactions;
         this.updateParent = updateParent;
+        this.maximumFactions = 7;
+        this.firstRender = true;
+        this.recentlyUnselectedFaction = '';
     }
 
     static get styles() {
@@ -124,9 +130,22 @@ class SelectFaction extends LitElement {
             height:100%;
             width:100%;
         }
+
+        .shake {
+            animation: shake .5s;
+        }
         
         .gray {
+            transition: filter .3s ease-out;
             filter: grayscale(90%);
+        }
+
+        @keyframes shake {
+            0% { transform: translate(10px, 0px); }
+            25% { transform: translate(-10px, 0px); }
+            50% { transform: translate(15px, 0px); }
+            75% { transform: translate(-5px, 0px); }
+            100% { transform: translate(0px, 0px); }
         }
         ${btnCSS()}`
     }
@@ -134,13 +153,31 @@ class SelectFaction extends LitElement {
     displayFactions = () => {
         return this.factions.map(v => {
             let path = icons[`${v}_icon`];
-            return html`<img draggable='false' class=${this.selectedFactions[v] ? 'faction-icon' : 'faction-icon gray' } src='${path}'
+            let shake = this.recentlyUnselectedFaction === v ? ' shake' : '';
+            return html`<img draggable='false' 
+            class=${this.selectedFactions[v] ? 'faction-icon' : 'faction-icon gray' + shake }
+            src='${path}'
     @click=${()=> { this.selectFaction(v) }} ></img>`;
         });
     }
 
     selectFaction = (faction: string) => {
-        this.selectedFactions[faction] ? delete this.selectedFactions[faction] : this.selectedFactions[faction] = true;
+        if (this.selectedFactions[faction]) {
+            this.recentlyUnselectedFaction = faction;
+            delete this.selectedFactions[faction];
+        } else {
+            this.selectedFactions[faction] = true;
+        }
+
+        const keys = Object.keys(this.selectedFactions);
+
+        if (keys.length > this.maximumFactions) {
+            this.recentlyUnselectedFaction = keys[0];
+            delete this.selectedFactions[keys[0]];
+        }
+
+        console.log(this.recentlyUnselectedFaction);
+
         this.updateParent(this.selectedFactions);
         // need to create directive to handle updating
         // https://codepen.io/sorvell/pen/BeLqMN?editors=0010
@@ -153,7 +190,12 @@ class SelectFaction extends LitElement {
             <div class='faction-grid'>
                 ${this.displayFactions()}
             </div>
-            <button class='glow-button' @click=${()=> { this.next() }}>Next</button>
+            ${Object.keys(this.selectedFactions).length > 0 ?
+                html`<button class='glow-button' @click=${()=> { this.next() }}>Next</button>`
+            :   
+                html`<button disabled class='glow-button' ></button>`
+            
+            }
         `
     }
 }
@@ -239,15 +281,29 @@ class SelectBoard extends LitElement {
 
     selectBoard(board: string) {
         const faction = this.factionsToCycle[this.currentFaction];
+
         if (this.currentlySelectedBoard === board) {
             this.boards[board] = '';
             this.currentlySelectedBoard = '';
         } else {
-            this.boards[board] = faction;
+            // when current faction is assigned a board
+            // check if the new board belongs to another faciton
+            // if so, switch their boards else assign a blank board
+            if (this.boards[board] && this.boards[board] !== faction) {
+                if (this.pairs[faction]) {
+                    let newBoard = this.pairs[faction];
+                    let otherFaction = this.boards[board];
 
-            if (this.pairs[faction]) {
-                this.boards[this.pairs[faction]] = '';
+                    this.boards[newBoard] = otherFaction;
+                    this.pairs[otherFaction] = newBoard;
+                }
+            } else {
+                if (this.pairs[faction]) {
+                    this.boards[this.pairs[faction]] = '';
+                }
             }
+
+            this.boards[board] = faction;
 
             this.currentlySelectedBoard = board;
         }
@@ -385,6 +441,7 @@ export default class NewGame extends LitElement {
         this.steps = [
             this.displaySelectFaction,
             this.displaySelectBoard,
+            this.displayScoreBoard
         ]
     }
 
@@ -394,6 +451,10 @@ export default class NewGame extends LitElement {
 
     displaySelectBoard = () => {
         return new SelectBoard(this.goToNextStep, this.getSelectedFactions, this.updateBoardPairs);
+    }
+
+    displayScoreBoard = () => {
+
     }
 
     goToNextStep = () => {
